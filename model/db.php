1606671148e608d759b66c2caab63687e6da7fd3
Unknown <Deadstylebp@gmail.com>
2020-03-13 18:55:17 -0500

    Update DB for Heroku

--- model/db.php
+++ model/db.php
@@ -1,11 +1,11 @@
 <?php
     // DB variables set to required settings
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-
+    $dsn = 'mysql:host=dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=slpafkyj9hk6x3t2';
+    $username = 'mw3yn3xjtchrilr8';
+    $password = 't5c53slvfyas7c2l';
     // Try DBO connection
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         // Display error message
         $error_message = $e->getMessage();
